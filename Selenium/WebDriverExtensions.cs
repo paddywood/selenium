3e9ddb70767cd71069b6838f1cb56e59f3f85281
Paddy <pfmwood@gmail.com>
2018-12-04 18:37:41 +1300

    stuff

--- Selenium/WebDriverExtensions.cs
+++ Selenium/WebDriverExtensions.cs
@@ -11,6 +11,21 @@ namespace Selenium
             return FindElement(d => d.FindElement(By.Id(id)), webDriver, timeout);
         }
 
+        public static IWebElement FindElementByName(this IWebDriver webDriver, string name, int timeout = 3000)
+        {
+            return FindElement(d => d.FindElement(By.Name(name)), webDriver, timeout);
+        }
+
+        public static IWebElement FindElementByCssSelector(this IWebDriver webDriver, string cssSelector, int timeout = 3000)
+        {
+            return FindElement(d => d.FindElement(By.CssSelector(cssSelector)), webDriver, timeout);
+        }
+
+        public static IWebElement FindElementByClassName(this IWebDriver webDriver, string className, int timeout = 3000)
+        {
+            return FindElement(d => d.FindElement(By.ClassName(className)), webDriver, timeout);
+        }
+
         private static IWebElement FindElement(Func<IWebDriver, IWebElement> eleFunc, IWebDriver webDriver, int timeout)
         {
             var wait = new WebDriverWait(webDriver, new TimeSpan(timeout));
